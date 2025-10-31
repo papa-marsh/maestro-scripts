d1573c522fc8f1fc41f075a724aa414eb4655905
Marshall Warners <m@marshallwarners.com>
2025-10-31 08:00:49 -0400

    Typo

--- system/backups.py
+++ system/backups.py
@@ -5,7 +5,7 @@ from maestro.utils import Notif
 
 @cron_trigger(hour=8)
 def check_cloud_backup_state() -> None:
-    if sensor.backup_state != "backed_up":
+    if sensor.backup_state.state != "backed_up":
         noti = Notif(
             title="Cloud Backup Failed",
             message="Google Drive backup appears to have failed - Check add-on",
