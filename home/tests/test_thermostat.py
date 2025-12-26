e9e6a6287a7cf467d4f9536db7b84865807b7052
Marshall Warners <m@marshallwarners.com>
2025-12-25 22:59:41 -0500

    Thermostat hold reminder

--- home/tests/test_thermostat.py
+++ home/tests/test_thermostat.py
@@ -1,9 +1,54 @@
+from maestro.domains import HOME
 from maestro.integrations import Domain
-from maestro.registry import climate, person
+from maestro.registry import climate, person, zone
 from maestro.testing import MaestroTest
 from scripts.custom_domains.climate import Thermostat
 
 from .. import thermostat
+
+lakeshore_zone_name = "Grand Haven"
+
+
+def test_thermostat_hold_reminder(mt: MaestroTest) -> None:
+    # Notif not sent when Marshall is not at lakeshore
+    mt.set_state(person.marshall, HOME)
+    mt.set_state(person.emily, "Grand Haven")
+    mt.set_state(
+        entity=climate.thermostat,
+        state=Thermostat.HVACMode.HEAT,
+        attributes={"preset_mode": Thermostat.PresetMode.NONE},
+    )
+    thermostat.thermostat_hold_reminder()
+    mt.assert_action_not_called(Domain.NOTIFY, person.marshall.notify_action_name)
+
+    # Notif not sent when Emily is not at lakeshore
+    mt.set_state(person.marshall, "Grand Haven")
+    mt.set_state(person.emily, HOME)
+    thermostat.thermostat_hold_reminder()
+    mt.assert_action_not_called(Domain.NOTIFY, person.marshall.notify_action_name)
+
+    # Notif not sent when thermostat is on hold
+    mt.set_state(person.marshall, "Grand Haven")
+    mt.set_state(person.emily, "Grand Haven")
+    mt.set_state(
+        entity=climate.thermostat,
+        state=Thermostat.HVACMode.HEAT,
+        attributes={"preset_mode": Thermostat.PresetMode.HOLD},
+    )
+    thermostat.thermostat_hold_reminder()
+    mt.assert_action_not_called(Domain.NOTIFY, person.marshall.notify_action_name)
+
+    # Notif sent when both at lakeshore and thermostat not on hold
+    mt.set_state(person.marshall, "Grand Haven")
+    mt.set_state(person.emily, "Grand Haven")
+    mt.set_state(
+        entity=climate.thermostat,
+        state=Thermostat.HVACMode.HEAT,
+        attributes={"preset_mode": Thermostat.PresetMode.NONE, "temperature": 68},
+    )
+    thermostat.thermostat_hold_reminder()
+    mt.assert_action_called(Domain.NOTIFY, person.marshall.notify_action_name)
+    mt.assert_action_not_called(Domain.NOTIFY, person.emily.notify_action_name)
 
 
 def test_check_thermostat_hold(mt: MaestroTest) -> None:
--- home/thermostat.py
+++ home/thermostat.py
@@ -2,6 +2,26 @@ from maestro.registry import climate, person
 from maestro.triggers import cron_trigger
 from maestro.utils import Notif
 from scripts.custom_domains.climate import Thermostat
+from scripts.custom_domains.zone import ZoneExtended
+
+
+@cron_trigger(hour=20)
+def thermostat_hold_reminder() -> None:
+    marshall_at_lakeshore = ZoneExtended.get_zone_metadata(person.marshall.state).lakeshore
+    emily_at_lakeshore = ZoneExtended.get_zone_metadata(person.emily.state).lakeshore
+
+    if (
+        marshall_at_lakeshore
+        and emily_at_lakeshore
+        and climate.thermostat.preset_mode != Thermostat.PresetMode.HOLD
+    ):
+        Notif(
+            title="Thermostat on Auto",
+            message=(
+                f"The thermostat is set to auto mode at {climate.thermostat.temperature}°. "
+                "Consider setting a hold at a more conservative setpoint to save energy."
+            ),
+        ).send(person.marshall)
 
 
 @cron_trigger(hour=8)
