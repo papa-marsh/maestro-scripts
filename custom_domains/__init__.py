384f6bebde73071499d8b18c398e263590233f64
Marshall Warners <m@marshallwarners.com>
2025-12-30 10:10:57 -0500

    Add GoogleCalendar to custom_domain init

--- custom_domains/__init__.py
+++ custom_domains/__init__.py
@@ -1,5 +1,6 @@
 from .climate import BathroomFloor, TeslaHVAC, Thermostat
 from .complication import GaugeTextComplication
+from .google_calendar import GoogleCalendar
 from .sonos_speaker import SonosSpeaker
 from .sprinkler_zone import SprinklerZone
 from .zone import ZoneExtended
@@ -8,6 +9,7 @@ __all__ = [
     BathroomFloor.__name__,
     TeslaHVAC.__name__,
     GaugeTextComplication.__name__,
+    GoogleCalendar.__name__,
     Thermostat.__name__,
     SonosSpeaker.__name__,
     SprinklerZone.__name__,
