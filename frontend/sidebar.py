e6fb8db993a761dcb5e9c8b1d4365de9718fc70a
Marshall Warners <m@marshallwarners.com>
2026-01-27 08:18:17 -0500

    Simplify sidebr text

--- frontend/sidebar.py
+++ frontend/sidebar.py
@@ -1,38 +1,21 @@
 from datetime import date
 
-from maestro.integrations import EntityId, StateManager
-from maestro.registry import calendar, maestro
+from maestro.registry import maestro
 from maestro.triggers import HassEvent, MaestroEvent, cron_trigger, hass_trigger, maestro_trigger
-from scripts.custom_domains.google_calendar import GoogleCalendar
-
-calendar_ids: list[GoogleCalendar] = [
-    calendar.family.id,
-    calendar.marshall.id,
-    calendar.emily.id,
-    calendar.ellie.id,
-    calendar.olivia.id,
-]
 
 
 @hass_trigger(HassEvent.STARTUP)
 @maestro_trigger(MaestroEvent.STARTUP)
-def initialize_sidebar_text_entity() -> None:
-    StateManager().initialize_hass_entity(
-        entity_id=EntityId("maestro.cast_sidebar_text"),
-        state=build_sidebar_text(),
-        attributes={},
-    )
-
-
 @cron_trigger(hour=0)
 def set_sidebar_text() -> None:
-    maestro.cast_sidebar_text.state = build_sidebar_text()
-
-
-def build_sidebar_text() -> str:
     today = date.today().strftime("%A")
-
-    return f"""
+    sidebar_text = f"""
         <li>Happy {today}!</li>
         <li>‎</li>
     """
+
+    maestro.cast_sidebar_text.state_manager.post_hass_entity(
+        entity_id=maestro.cast_sidebar_text.id,
+        state=sidebar_text,
+        attributes={},
+    )
