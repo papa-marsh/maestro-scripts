60dd931c832a5ab7fd33c8c2725c63627ab60569
Marshall Warners <m@marshallwarners.com>
2026-01-27 15:00:40 -0500

    Call cast command regardless of state; swallow client errors

--- frontend/cast.py
+++ frontend/cast.py
@@ -4,6 +4,7 @@ from maestro.domains import MediaPlayer
 from maestro.integrations import Domain, RedisClient
 from maestro.registry import media_player
 from maestro.triggers import cron_trigger
+from maestro.utils import exceptions, log
 
 NEST_DISPLAYS: list[MediaPlayer] = [
     media_player.office_display,
@@ -15,29 +16,19 @@ CAST_LOCK_KEY = "cast_lock"
 
 
 def call_cast_command(display: MediaPlayer) -> None:
-    display.state_manager.hass_client.perform_action(
-        domain=Domain.SHELL_COMMAND,
-        action=f"cast_to_{display.id.entity}",
-        entity_id=display.id,
-    )
+    try:
+        display.state_manager.hass_client.perform_action(
+            domain=Domain.SHELL_COMMAND,
+            action=f"cast_to_{display.id.entity}",
+            entity_id=display.id,
+        )
+    except exceptions.HomeAssistantClientError:
+        log.info("Attempted to cast and got HomeAssistantClientError", target=display.id)
 
 
 @cron_trigger("*/10 * * * *")
-def cast_to_displays(force_reset: bool = False) -> None:
-    with RedisClient().lock(CAST_LOCK_KEY, timeout_seconds=180, exit_if_owned=True):
+def cast_to_displays() -> None:
+    with RedisClient().lock(CAST_LOCK_KEY, timeout_seconds=60, exit_if_owned=True):
         for display in NEST_DISPLAYS:
-            if force_reset and display.state == "on":
-                display.turn_off()
-                sleep(5)
-            if force_reset or display.state == "off":
-                display.turn_on()
-                sleep(5)
-                display.turn_off()
-                sleep(5)
-                call_cast_command(display)
-                sleep(30)
-
-
-@cron_trigger(hour=3)
-def force_cast_reset() -> None:
-    cast_to_displays(force_reset=True)
+            call_cast_command(display)
+            sleep(60)
