adbf8a6ccc9c13dd6b1cc36bbfaa017f30c1723a
Marshall Warners <m@marshallwarners.com>
2026-01-27 15:00:57 -0500

    Re-add sunrise/sunset time to sidebar text

--- frontend/sidebar.py
+++ frontend/sidebar.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from maestro.registry import maestro
+from maestro.registry import maestro, sun
 from maestro.triggers import HassEvent, MaestroEvent, cron_trigger, hass_trigger, maestro_trigger
 
 
@@ -9,9 +9,17 @@ from maestro.triggers import HassEvent, MaestroEvent, cron_trigger, hass_trigger
 @cron_trigger(hour=0)
 def set_sidebar_text() -> None:
     today = date.today().strftime("%A")
+
+    if sun.sun.is_above_horizon:
+        sun_action = "sets"
+        sun_time = sun.sun.next_setting.strftime("%-I:%M %p")
+    else:
+        sun_action = "rises"
+        sun_time = sun.sun.next_rising.strftime("%-I:%M %p")
+
     sidebar_text = f"""
         <li>Happy {today}!</li>
-        <li>‎</li>
+        <li>The sun {sun_action} at {sun_time}.</li>
     """
 
     maestro.cast_sidebar_text.state_manager.post_hass_entity(
