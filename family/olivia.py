de0d9e9b0ca3f34bf8423382e702a6cc30f74e85
Marshall Warners <m@marshallwarners.com>
2026-02-02 12:28:12 -0500

    Don't turn on sound machine if not home

--- family/olivia.py
+++ family/olivia.py
@@ -1,10 +1,13 @@
-from maestro.registry import switch
+from maestro.registry import person, switch
 from maestro.triggers import event_fired_trigger
 from scripts.common.event_type import EventType
 
 
 @event_fired_trigger(EventType.OLIVIA_ASLEEP)
 def sound_machine_on() -> None:
+    if not person.emily.is_home:
+        return
+
     switch.olivias_sound_machine.turn_on()
 
 
