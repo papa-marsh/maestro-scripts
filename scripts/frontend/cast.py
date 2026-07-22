bbe2b33f1b5a7a2b4da3b9f4f57766c7fec1c8d4
Marshall Warners <m@marshallwarners.com>
2026-07-22 09:20:26 -0400

    Add concurrent.futures timeout to cast commands

--- scripts/frontend/cast.py
+++ scripts/frontend/cast.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 from time import sleep
 
 from catt.controllers import setup_cast  # type:ignore[import-untyped]
@@ -16,22 +17,35 @@ NEST_DISPLAYS: list[tuple[MediaPlayer, str]] = [
 
 CAST_URL = "http://192.168.0.107:8123/lovelace-cast/overview"
 CAST_LOCK_KEY_PREFIX = "cast_lock_"
+CAST_TIMEOUT_SECONDS = 60
+
+
+def execute_cast(ip_address: str) -> None:
+    cast_controller = setup_cast(
+        ip_address,
+        controller="dashcast",
+        action="load_url",
+        prep="app",
+    )
+    cast_controller.load_url(CAST_URL)
 
 
 def call_cast_command(display: MediaPlayer, ip_address: str) -> None:
     lock_key = CAST_LOCK_KEY_PREFIX + display.id.entity
     with RedisClient().lock(lock_key, timeout_seconds=100, exit_if_owned=True):
+        executor = ThreadPoolExecutor(max_workers=1)
         try:
-            cast_controller = setup_cast(
-                ip_address,
-                controller="dashcast",
-                action="load_url",
-                prep="app",
+            executor.submit(execute_cast, ip_address).result(timeout=CAST_TIMEOUT_SECONDS)
+        except TimeoutError:
+            log.error(
+                "Timed out while attempting to cast",
+                target=display.id,
+                timeout_seconds=CAST_TIMEOUT_SECONDS,
             )
-            cast_controller.load_url(CAST_URL)
-
         except Exception:
             log.exception("Exception raised while attempting to cast", target=display.id)
+        finally:
+            executor.shutdown(wait=False, cancel_futures=True)
 
 
 @cron_trigger("*/10 * * * *")
